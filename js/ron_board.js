8640268b4f37ff483fba78c31f9e4809618064c4
ronammar <ron.ammar@mail.utoronto.ca>
2014-06-22 03:20:30 -0400

    Full size simple board now implemented in playing_with_drawingboard.html.

--- js/ron_board.js
+++ js/ron_board.js
@@ -1,3 +1,7 @@
+// Set the board dimensions over here BEFORE creating a new simple board
+$('#simple-board').width(window.innerWidth);
+$('#simple-board').height(window.innerHeight);
+
 var simpleBoard = new DrawingBoard.Board('simple-board', {
 	controls: false,
 	webStorage: false
